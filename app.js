1213cd946cb78edf331621bd5e2135b72845757a
LuigiCasale95 <luigicasale95@gmail.com>
2025-10-16 15:26:29 +0200

    correzione posts.js

--- app.js
+++ app.js
@@ -1,33 +1,12 @@
-/* Esercizio,
-Creare un file di routing (routers/posts.js) che conterrà le rotte necessario per l'entità post.
-
-All'interno creare le rotte per le operazioni CRUD (Index, Show, Create, Update e Delete)
-
-Tutte le risposte saranno dei testi che confermeranno l’operazione che il server deve eseguire, secondo le convenzioni REST.
-
-Ad esempio: 
-
-Se viene chiamata /posts col verbo GET ci aspettiamo “Lista dei post”;
-
-Se viene chiamato /posts/1 col verbo DELETE ci aspettiamo “Cancellazione del post 1”
-
-e via dicendo…
-
-Registrare il router dentro app.js con il prefisso posts/.
-
-Nota:
-
-Avete anche l’array dei post che vi abbiamo fornito, salvatelo da qualche parte.
-Ci servirà per i prossimi step.
-Per oggi vi può servire in caso vogliate provare i bonus.
- */
-
 /* import del framework express */
 const express = require("express");
 /* Creazione instanza di express */
 const app = express();
 /* Impostiamo un ref per il numero della porta*/
 const port = 3000;
+
+// importiamo modulo router pizze
+const postsRouter = require("./router/posts.js")
 
 /*usiamo il middleware static di express (per remdere disponibile il file statico ovvero le img in questo caso)*/
 app.use(express.static("public"));
@@ -37,68 +16,9 @@ app.get("/", (req, res) => {
     res.send('<h1>Server del mio blog</h1>')
 })
 
-/* rotte di  CRUD sulla risorsa posts*/
-// index
-app.get('/posts', function (req, res) {
-res.send('Lista dei posts');
-});
-// show
-app.get('/posts/:id', function (req, res) {
-res.send('Dettagli del post ' + req.params.id);
-});
-// store
-app.post('/posts', function (req, res) {
-res.send('Creazione nuovo post');
-});
-// update
-app.put('/posts/:id', function (req, res) {
-res.send('Modifica integrale del post' + req.params.id);
-});
-// modify
-app.patch('/posts/:id', function (req, res) {
-res.send('Modifica parziale del post ' + req.params.id);
-});
-// destroy
-app.delete('/posts/:id', function (req, res) {
-res.send('Eliminazione del post ' + req.params.id);
-});
 
-/* creazione di una seconda rotta  */
-/* app.get("/bacheca", (req, res) => {
-    const contenuti = [
-        {
-            titolo: "prova",
-            contenuto: "prova contenuto",
-            image: "public\imgs\ciambellone.jpeg",
-            tags: ["prova 1", "prova 2", "prova 3"]
-        },
-        {
-            titolo: "prova",
-            contenuto: "prova contenuto",
-            image: "public\img\imgs\cracker_barbabietola.jpeg",
-            tags: ["prova 1", "prova 2", "prova 3"]
-        },
-        {
-            titolo: "prova",
-            contenuto: "prova contenuto",
-            image: "public\imgs\pane_fritto_dolce.jpeg",
-            tags: ["prova 1", "prova 2", "prova 3"]
-        },
-        {
-            titolo: "prova",
-            contenuto: "prova contenuto",
-            image: "public\imgs\pasta_barbabietola.jpeg",
-            tags: ["prova 1", "prova 2", "prova 3"]
-        },
-        {
-            titolo: "prova",
-            contenuto: "prova contenuto",
-            image: "public\imgs\torta_paesana.jpeg",
-            tags: ["prova 1", "prova 2", "prova 3"]
-        }
-    ];
-    res.json(contenuti)
-}) */
+// rotte per i post
+app.use("/posts", postsRouter);
 
 
 /* mettiamo in ascolto il server sulla porta definitiva */
